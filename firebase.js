cd9efcafc1ff75b3b06c517dd67cd0649cf5f304
vivo4488-ops <vivo4488@gmail.com>
2026-06-18 09:31:00 +0200

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,10 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBXaLcFO9bcm7DMn6UcYLFTQTlzhXhwiT0",
+  authDomain: "tiktok-radi.firebaseapp.com",
+  databaseURL: "https://tiktok-radi-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tiktok-radi",
+  storageBucket: "tiktok-radi.firebasestorage.app",
+  messagingSenderId: "480663171336",
+  appId: "1:480663171336:web:cf869ccc9e3e888ab3fcbd",
+  measurementId: "G-S1N62DWC3X"
+};
